aebe960b77983bbcfa0d16e2dc3ad067c37c5855
Flamir0908123 <bgladki86@gmail.com>
2021-10-31 16:49:15 +0300

    gg

--- js/cart.js
+++ js/cart.js
@@ -1,49 +1,109 @@
+"use strict";
 
-// Скрипт выбора типов розы (выделени блока зеленым + смена фото розы)
-$('.cart-options__item_type').click(function(){
+let lezh_type;
+let lezh_size;
 
-  $('.cart-options__item_type')
-  .removeClass('cart-options__item_selected')
-  .find('.cart-options__item-btn')
-  .removeClass('cart-options__item-btn_selected')
-  .text('Выбрать');
+function Type(el){
 
-  $(this).addClass('cart-options__item_selected')
-  .find('.cart-options__item-btn')
-  .addClass('cart-options__item-btn_selected')
-  .text('Выбрано');
+  $(".lezh_types").removeClass("selected-cart-type");
+  $(el).addClass("selected-cart-type");
 
-  // Меняем фото розы в зависимости какой блок был нажат
-  let roseType =  $(this).data('rose');
-  $('.cart-options__photo').addClass('cart-options__photo_hidden');
-  $('.cart-options__photo' + '[data-rose=' +  roseType + ']')
-  .removeClass('cart-options__photo_hidden');
-
-  // Разблокируем блок с выбором цвета розы
-  $('.cart-options__colors').css({'opacity':1});
-  $('.cart-options__item_colors').addClass('cart-options__item_active');
-
-});
-
-
-
-// Скрипт выделения кнопки в блоке ЭФФЕКТНЫЕ ДОПОЛНЕНИЯ К РОЗЕ
-$('#wow-box').click(function(){
-
-  let selectedText =  '<img src="img/icon-checked-black.svg" width="11" height="11" alt="">' + $(this).data('selected');
-
-  if ( $(this).hasClass('cart-addons__item-btn_selected') ) {
-    $(this).removeClass('cart-addons__item-btn_selected').text( $(this).data('not-selected') );
-  } else {
-    $(this).addClass('cart-addons__item-btn_selected').html(selectedText);
+  if($(el).hasClass("type_1")){
+    lezh_type = 1;
+  }
+  else{ 
+    lezh_type = 2;
   }
 
-});
+  ChangeLezh()
+
+}
+
+function Size(el){
+
+  $(".lezh_sizes").removeClass("selected-cart-size");
+  $(el).addClass("selected-cart-size");
+
+  if($(el).hasClass("size_1")){   
+    lezh_size = 1; 
+  }
+  else if($(el).hasClass("size_2")){
+    lezh_size = 2;  
+  }
+  else if($(el).hasClass("size_3")){
+    lezh_size = 3;
+  }
+  else{
+    lezh_size = 4;
+  }
+
+  ChangeLezh()
+
+  $(".size_svg").attr("src", "img/svg/green.svg");
+  $(".blur_step3").removeClass("cart_blur");
+  $(".color_svg").attr("src", "img/svg/icon_important.svg");
+} 
 
 
-// Вывод описания типа розы при клике по блокам (mini, premium, king)
-$('.cart-options__item_type').click(function(){
-  let curRoseType = $(this).data('rose');
-  $('.cart-options__rose-type').removeClass('cart-options__rose-type_active');
-  $(`.cart-options__rose-type[data-rose="${curRoseType}"]`).addClass('cart-options__rose-type_active');
-});
+
+
+function ChangeLezh(){
+ 
+  if(!lezh_size){
+    console.log(`Тип: ${lezh_type}`);
+    $(".type_svg").attr("src", "img/svg/green.svg");
+    $(".blur_step2").removeClass("cart_blur");
+    $(".size_svg").attr("src", "img/svg/icon_important.svg");
+    if(lezh_type==1){
+      $(".cart-options__photo").attr("src", "img/zagl/type1/default/Standart/1.jpg");
+      $(".cart-options__photo").attr("alt", `Тип: ${lezh_type}`);
+      $(".cart-options__rose-type_active").text("Круглая");
+    }
+    else{
+      $(".cart-options__photo").attr("src", "img/zagl/type1/default/Standart/1.jpg");
+      $(".cart-options__photo").attr("alt", `Тип: ${lezh_type}`);
+      $(".cart-options__rose-type_active").text("Прямоугольная");
+    }
+  }
+  else{
+    console.log(`Тип: ${lezh_type} | Размер: ${lezh_size}`);
+    if(lezh_type==1){
+      $(".cart-options__rose-type_active").text("Круглая");
+      if(lezh_size==1){   
+        $(".cart-options__photo").attr("src", "img/zagl/type1/size1/Standart/1.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else if(lezh_size==2){   
+        $(".cart-options__photo").attr("src", "img/zagl/type1/size2/Standart/2.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else if(lezh_size==3){ 
+        $(".cart-options__photo").attr("src", "img/zagl/type1/size3/Standart/3.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else{ 
+        $(".cart-options__photo").attr("src", "img/zagl/type1/default/Standart/4.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+    }
+    else{
+      $(".cart-options__rose-type_active").text("Прямоугольная");
+      if(lezh_size==1){   
+        $(".cart-options__photo").attr("src", "img/zagl/type2/size1/Standart/1.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else if(lezh_size==2){     
+        $(".cart-options__photo").attr("src", "img/zagl/type2/size2/Standart/2.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else if(lezh_size==3){
+        $(".cart-options__photo").attr("src", "img/zagl/type2/size3/Standart/3.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+      else{   
+        $(".cart-options__photo").attr("src", "img/zagl/type2/default/Standart/4.jpg");
+        $(".cart-options__photo").attr("alt", `Тип: ${lezh_type} | Размер: ${lezh_size}`);
+      }
+    }    
+  }
+}
